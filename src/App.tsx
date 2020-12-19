19b551831b92c2ecf60a8fe81bd88087be61e00a
Artem Strelbitskiy <artstrelb@yandex.ru>
2020-12-19 17:15:58 +0500

    Компонент ErrorBoundary для обработки ошибок

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import ErrorBoundary from "./components/ErrorBoundary";
 
-const App: React.FC = () => {
-    return (
-        <div>
-            Hello from Assteroids
-        </div>
-    )
+
+export default class App extends PureComponent {
+    public render() {
+        return (
+            <ErrorBoundary>
+                <div/>
+            </ErrorBoundary>
+        );
+    }
+  
 }
 
-export default App;
+
---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+interface state {
+  error: object | null,
+  errorInfo: React.ErrorInfo | null
+}
+
+export default class ErrorBoundary extends Component {
+  state: state = {
+    error: null,
+    errorInfo: null,
+  }
+  
+  componentDidCatch(error: object, errorInfo: React.ErrorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+  }
+  
+  render() {
+    const error = this.state.error;
+
+    const errorStr = error ? error.toString() : "";
+
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {errorStr}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    
+    return this.props.children;
+  }  
+}
