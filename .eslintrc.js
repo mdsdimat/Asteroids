70249a85781eb89ab12b904dd48b99ccaac7cf05
dmitrymatveev <mdsdimat@gmail.com>
2020-12-22 19:27:58 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,66 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.ts', '.js', '.tsx'],
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "airbnb"
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  ignorePatterns: [
+    'node_modules',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-props-no-spreading': ['error', {
+      html: 'ignore',
+      custom: 'ignore',
+      exceptions: [''],
+    }],
+    'no-unused-vars': 2,
+    'max-params': [2, 3],
+    indent: 2,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "settings":{
-        "import/resolver": {
-            "node": {
-                "paths": ["src"],
-                "extensions": [".ts", ".js"]
-            }
-        }
-    },
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "ignorePatterns": [
-        "node_modules"
-    ],
-    "rules": {
-        "no-unused-vars": 2,
-        "max-params": [2, 3],
-        "indent": ["error", 4],
-        "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-                "js": "never",
-                "ts": "never",
-            }
-        ],
-        "no-underscore-dangle": 0,
-        "camelcase": 0,
-        "linebreak-style": 0,
-        "class-methods-use-this": 0,
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        "no-continue": 0,
-        "no-param-reassign": 0,
-        "no-undef": 0,
-        "no-loop-func": 0,
-        "no-use-before-define": 0,
-        "no-shadow": 0,
-        "react/static-property-placement": "off",
-        "no-restricted-syntax": "off",
-        "no-prototype-builtins": "off",
-        '@typescript-eslint/no-var-requires': 0
-    }
+    'no-underscore-dangle': 0,
+    camelcase: 0,
+    'linebreak-style': 0,
+    'class-methods-use-this': 0,
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-continue': 0,
+    'no-param-reassign': 0,
+    'no-undef': 0,
+    'no-loop-func': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'react/static-property-placement': 'off',
+    'no-restricted-syntax': 'off',
+    'no-prototype-builtins': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 
-const App: React.FC = () => {
-    return (
-        <RegistrationForm/>
-    )
-}
+const App: React.FC = () => (
+  <RegistrationForm />
+);
 
 export default App;
