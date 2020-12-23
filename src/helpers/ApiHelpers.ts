44fa03acb8a74befd2f854c0709eaf02a10ea903
ButtSurfer <fisher10058@mail.ru>
2020-12-23 10:58:24 +0300

    Апиха для регистрации/логина + типы

---
+++ src/helpers/ApiHelpers.ts
@@ -0,0 +1,5 @@
+const basePath = 'https://ya-practicum.tech/api/v2/';
+
+export const buildUrl = (path: string) => {
+    return basePath+path
+}
---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+export interface SignUpRequest {
+    first_name: string;
+    second_name: string;
+    login: string;
+    password: string;
+    phone: string;
+}
+
+export interface SignUser {
+    login: string;
+    password: string;
+}
+
+export interface UserResponse {
+    id: number;
+    first_name: string;
+    second_name: string;
+    display_name: string;
+    login: string;
+    email: string;
+    phone: string;
+    avatar: string;
+}
+
+export interface LeaderboardNewLeaderRequest {
+    data: unknown;
+    ratingFieldName: string;
+}
