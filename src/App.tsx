1d7f0f4b23cb487cb491615e03aef55082a9bc06
Artem Strelbitskiy <artstrelb@yandex.ru>
2020-12-24 18:23:07 +0500

    Добавил стандартный шаблон страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,63 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
 } from 'react-router-dom';
 
-import ProfileForm from "./components/ProfileForm";
+import { Layout, Menu } from 'antd';
+
+import ProfileForm from './components/ProfileForm';
+import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 
+const { Header, Footer, Content } = Layout;
+
 const App: React.FC = () => {
-    return (
-        <div>
-            <Router>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Главная</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">авторизация</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">регистрация</Link>
-                        </li>
-                        <li>
-                            <Link to="/404">404</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/" exact>Главная она же страница игры</Route>
-                    <Route path="/login" component={ProfileForm} />
-                    <Route path="/register" component={RegistrationForm} />
-                    <Route path="/about">О компании</Route>
-                    <Route>Здесь будет 404</Route>
-                </Switch>
-            </Router>
-        </div>
-    )
+  return (
+      <>
+        <Layout>
+          <Router>
+            <Header>
+              <div className="logo" />
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1">
+                    Играть
+                    <Link to="/" />
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    Регистрация
+                    <Link to="/register" />
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    Профиль
+                    <Link to="/profile" />
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    Доска почета
+                    <Link to="/dashboard" />
+                  </Menu.Item>
+              </Menu>
+            </Header>
+            <Content>
+              <Switch>
+                <Route path="/" exact>Главная она же страница игры</Route>
+                  <Route path="/login" component={LoginForm} />
+                  <Route path="/register" component={RegistrationForm} />
+                  <Route path="/profile" component={ProfileForm}>Профиль</Route>
+                  <Route path="/dashboard">Доска</Route>
+                  <Route>
+                    <Redirect to="/login" />
+                  </Route>
+              </Switch>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Game &copy; 2020 Created by Helsinki</Footer>
+          </Router>
+        </Layout>
+      </>
+  )
 }
 
 export default App;
