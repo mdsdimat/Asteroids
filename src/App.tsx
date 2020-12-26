376912d974fcac5c7d0b5d525be6f32201999fab
dmitrymatveev <mdsdimat@gmail.com>
2020-12-26 22:04:55 +0300

    Страницы для форума

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
   Switch,
   Route,
   Redirect,
-  Link
+  Link,
 } from 'react-router-dom';
 
 import { Layout, Menu, Col } from 'antd';
@@ -13,56 +13,62 @@ import ProfileForm from './components/ProfileForm';
 import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 import Leaderboard from './components/Leaderboard/Leaderboard';
+import ForumList from './components/Forum/ForumList';
+import ForumPage from './components/Forum/ForumPage/ForumPage';
 
 const { Header, Footer, Content } = Layout;
 
-const App: React.FC = () => {
-  return (
-    <>
-      <Layout>
-        <Router>
-          <Col span={12} offset={6}>
+const App: React.FC = () => (
+  <>
+    <Layout>
+      <Router>
+        <Col span={12} offset={6}>
           <Header>
             <div className="logo" />
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                  Играть
-                  <Link to="/" />
-                </Menu.Item>
-                <Menu.Item key="2">
-                  Вход
-                  <Link to="/login" />
-                </Menu.Item>
-                <Menu.Item key="3">
-                  Регистрация
-                  <Link to="/register" />
-                </Menu.Item>
-                <Menu.Item key="4">
-                  Профиль
-                  <Link to="/profile" />
-                </Menu.Item>
-                <Menu.Item key="5">
-                  Доска почета
-                  <Link to="/dashboard" />
-                </Menu.Item>
+              <Menu.Item key="1">
+                Играть
+                <Link to="/" />
+              </Menu.Item>
+              <Menu.Item key="2">
+                Вход
+                <Link to="/login" />
+              </Menu.Item>
+              <Menu.Item key="3">
+                Регистрация
+                <Link to="/register" />
+              </Menu.Item>
+              <Menu.Item key="4">
+                Профиль
+                <Link to="/profile" />
+              </Menu.Item>
+              <Menu.Item key="5">
+                Доска почета
+                <Link to="/dashboard" />
+              </Menu.Item>
+              <Menu.Item key="6">
+                Форум
+                <Link to="/forum" />
+              </Menu.Item>
             </Menu>
           </Header>
-          </Col>
-          <Content>
-            <Switch>
-              <Route path="/" exact>Главная она же страница игры</Route>
-                <Route path="/login" component={LoginForm} exact />
-                <Route path="/register" component={RegistrationForm} exact />
-                <Route path="/profile" component={ProfileForm} exact />
-                <Route path="/dashboard" component={Leaderboard} exact />
-                <Redirect to="/login" />
-            </Switch>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Game &copy; 2020 Created by Helsinki</Footer>
-        </Router>
-      </Layout>
-    </>
-  )
-}
+        </Col>
+        <Content>
+          <Switch>
+            <Route path="/" exact>Главная она же страница игры</Route>
+            <Route path="/login" component={LoginForm} exact />
+            <Route path="/register" component={RegistrationForm} exact />
+            <Route path="/profile" component={ProfileForm} exact />
+            <Route path="/dashboard" component={Leaderboard} exact />
+            <Route path="/forum" component={ForumList} exact />
+            <Route path="/forum-page/:id" component={ForumPage} exact />
+            <Redirect to="/login" />
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Game &copy; 2020 Created by Helsinki</Footer>
+      </Router>
+    </Layout>
+  </>
+);
 
 export default App;
