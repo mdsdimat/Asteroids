1f478f157805484be2a469ada8dd37e6c99b7541
dmitrymatveev <mdsdimat@gmail.com>
2021-02-04 17:52:45 +0300

    yandex oAuth

--- src/store/sagas/auth.ts
+++ src/store/sagas/auth.ts
@@ -1,10 +1,10 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import AuthApi from '../../api/AuthApi';
-import { SignUser } from '../../types/types';
+import { IYandexOAuth, SignUser } from '../../types/types';
 import authActions from '../actions/auth';
 import {
-  getUserSuccess, requestAuth, requestAuthError, requestAuthSuccess,
+  getUserSuccess, requestAuth, requestAuthError, requestAuthSuccess, yandexAuthSuccess,
 } from '../actionCreators/auth';
 
 interface IAuthSaga {
@@ -15,6 +15,7 @@ interface IAuthSaga {
 export default function* watchLogin(): SagaIterator {
   yield takeEvery(authActions.login, loginAsync);
   yield takeEvery(authActions.getUser, getUserAsync);
+  yield takeEvery(authActions.yandexAuth, yandexAuthAsync);
 }
 
 function* loginAsync(sagaData: IAuthSaga) {
@@ -46,5 +47,18 @@ function* getUserAsync() {
     if (error.response.status !== 401) {
       yield put(requestAuthError(error));
     }
+  }
+}
+
+function* yandexAuthAsync(sagaData: IYandexOAuth) {
+  try {
+    yield put(requestAuth());
+    const data = yield call(() => AuthApi.oAuth(sagaData.code)
+      .then(
+        () => true,
+      ));
+    yield put(yandexAuthSuccess(data));
+  } catch (error) {
+    yield put(requestAuthError(error));
   }
 }
