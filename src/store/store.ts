f8e9c3964a4dd2e3ffd3c439612a5db8aaf61db7
Artem Strelbitskiy <artstrelb@yandex.ru>
2021-02-20 14:29:15 +0500

    Добавил redux-logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import createSagaMiddleware, { END } from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory, createMemoryHistory } from 'history';
+import { createLogger } from 'redux-logger';
 import { isServer } from '../helpers/isServer';
 
 import watchGotLeaderboard from './sagas/leaderboard';
@@ -10,6 +11,8 @@ import watchLogin from './sagas/auth';
 import createRootReducer from './reducers';
 
 import { AppStore, State } from '../types';
+
+import { IS_DEV } from '../../webpack/env';
 
 // eslint-disable-next-line import/prefer-default-export
 export function configureStore(initialState: State, url = '/') {
@@ -20,11 +23,17 @@ export function configureStore(initialState: State, url = '/') {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [routerMiddleware(history), sagaMiddleware];
 
+  if (IS_DEV) {
+    const logger = createLogger({ collapsed: true });
+    middlewares.push(logger);
+  }
+
   const store = createStore(
     createRootReducer(history),
     initialState,
     applyMiddleware(...middlewares),
   ) as AppStore;
+  store.dispatch({ type: '@@redux/INIT' });
 
   store.runSaga = sagaMiddleware.run;
   store.close = () => store.dispatch(END);
